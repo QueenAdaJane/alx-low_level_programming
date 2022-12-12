0b789eb51e11863716453f768fe9705d51bd7ecc
QueenAdaJane <toochyonwukwe014@gmail.com>
2022-12-12 11:40:05 -0800

    task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,11 +9,11 @@ int main(void)
 {
 	char letter;
 
-		for (letter != 'a'; letter  <= 'z'; letter++)
-		{
-			if (letter != 'e' && letter != 'q')
-				putchar(letter);
-		}
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'e' && letter != 'q')
+			putchar(letter);
+	}
 
 	putchar('\n');
 
