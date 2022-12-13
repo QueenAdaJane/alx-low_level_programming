789b1dd2f58507f3ee75ae25d9c119852c6348ae
QueenAdaJane <toochyonwukwe014@gmail.com>
2022-12-13 14:40:03 -0800

    task 0

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,26 +1,15 @@
 #include "main.h"
 
 /**
- * print_sign - Prints the sign of a number 
- * @n: The number to be checked
  *
- * Return: 1 for positive num, -1 for negative num or zero for anything else
+ * _abs - a function that computes the absolute value of an integer
+ * @ab: integer input
+ * Return: absolute value of ab
  */
-int print_sign(int n)
+
+int _abs(int ab)
+
 {
-	if (n > 0)
-	{
-		_putchar(43);
-		return (1);
-	}
-	else if (n < 0)
-	{
-		_putchar(45);
-		return (-1);
-	}
-	else
-	{
-		_putchar(48);
-		return (0);
-	}
+		return (ab * ((ab > 0) - (ab < 0)));
+
 }
