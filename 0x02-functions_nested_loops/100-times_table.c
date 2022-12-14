4ea7830a2c9af0266309b0002ba9142ea2d20b44
QueenAdaJane <toochyonwukwe014@gmail.com>
2022-12-14 14:27:42 -0800

    task 0

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,15 +7,12 @@
 void print_times_table(int n)
 
 {
-	int i, j, res :
-
-		if (!(n > 15 \\ n < 0))
+	int i, j, res;
+		if (!(n > 15 || n < 0))
 		{
-			for (i = 0; i <= n; i++)
-
+			for (i = 0; i <= n; ++)
 			{
 				for (j = 0; j <= n; j++)
-
 				{
 					res = (i * j);
 					if (j != 0)
