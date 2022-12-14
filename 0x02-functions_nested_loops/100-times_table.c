79926107a0bbec0b0e92f7428128fe96b2156166
QueenAdaJane <toochyonwukwe014@gmail.com>
2022-12-14 14:32:43 -0800

    task 1

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -30,7 +30,7 @@ void print_times_table(int n)
 					else if (res >= 10 && res < 100)
 					{
 						_putchar(' ');
-						_putcha((res / 10) + '0');
+						_putchar((res / 10) + '0');
 						_putchar((res % 10) + '0');
 					}
 					else if (res >= 100 && j !-0)
